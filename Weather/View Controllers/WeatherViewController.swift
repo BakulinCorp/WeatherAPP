b7363768fa668a1a9c307183b74ee95bc9cc6612
BakulinCorp <vjmax100500@gmail.com>
2021-09-28 11:20:03 +0300

    API 0.3.1

--- Weather/View Controllers/WeatherViewController.swift
+++ Weather/View Controllers/WeatherViewController.swift
@@ -92,7 +92,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
             // Convert data to models/some object
             
             var json: WeatherResponse?
-            print(WeatherResponse.self)
+//            print(WeatherResponse.self)
             do {
                 json = try JSONDecoder().decode(WeatherResponse.self, from: data)
             }
@@ -104,7 +104,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
                 return
             }
             
-            print(result.current.temp)
+//            print(result.current.temp)
             
             let entries = result.daily
             
@@ -116,6 +116,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
             
             
             self.hourlyModels = result.hourly
+            
             // Update user interface
             DispatchQueue.main.async {
                 self.table.reloadData()
@@ -130,26 +131,26 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
 
 
 // MARK - 24min
-
-// This file was generated from JSON Schema using quicktype, do not modify it directly.
-// To parse the JSON, add this file to your project and do:
-//
-//   let weatherResponse = try? newJSONDecoder().decode(WeatherResponse.self, from: jsonData)
-
+ 
 
 // MARK: - WeatherResponse
+
+
+import Foundation
+
+
 struct WeatherResponse: Codable {
     let lat, lon: Double
     let timezone: String
-    let timezoneOffset: Int
+    let timezoneOffset: Int?
     let current: Current
     let minutely: [Minutely]
     let hourly: [Current]
     let daily: [Daily]
-    
+
     enum CodingKeys: String, CodingKey {
         case lat, lon, timezone
-        case timezoneOffset = "timezone_offset"
+        case timezoneOffset
         case current, minutely, hourly, daily
     }
 }
@@ -158,27 +159,26 @@ struct WeatherResponse: Codable {
 struct Current: Codable {
     let dt: Int
     let sunrise, sunset: Int?
-    let temp: Double
-    let feelsLike: Double
+    let temp, feelsLike: Double?
     let pressure, humidity: Int
-    let dewPoint, uvi: Double
+    let dewPoint, uvi: Double?
     let clouds, visibility: Int
-    let windSpeed: Double
-    let windDeg: Int
-    let windGust: Double
+    let windSpeed: Double?
+    let windDeg: Int?
+    let windGust: Double?
     let weather: [Weather]
     let rain: Rain?
     let pop: Double?
 
     enum CodingKeys: String, CodingKey {
         case dt, sunrise, sunset, temp
-        case feelsLike = "feels_like"
+        case feelsLike
         case pressure, humidity
-        case dewPoint = "dew_point"
+        case dewPoint
         case uvi, clouds, visibility
-        case windSpeed = "wind_speed"
-        case windDeg = "wind_deg"
-        case windGust = "wind_gust"
+        case windSpeed
+        case windDeg
+        case windGust
         case weather, rain, pop
     }
 }
@@ -186,67 +186,67 @@ struct Current: Codable {
 // MARK: - Rain
 struct Rain: Codable {
     let the1H: Double
-    
+
     enum CodingKeys: String, CodingKey {
-        case the1H = "1h"
+        case the1H
     }
 }
 
 // MARK: - Weather
 struct Weather: Codable {
     let id: Int
-    let main: Main
-    let weatherDescription: String
-    let icon: Icon
-    
+//    let main: Main?
+    let weatherDescription: String?
+//    let icon: Icon?
+
     enum CodingKeys: String, CodingKey {
-        case id, main
-        case weatherDescription = "description"
-        case icon
+        case id
+        case weatherDescription
+//        case icon
     }
 }
 
-enum Icon: String, Codable {
-    case the02D = "02d"
-    case the03D = "03d"
-    case the04D = "04d"
-    case the04N = "04n"
-    case the09D = "09d"
-    case the10D = "10d"
-}
-
-enum Main: String, Codable {
-    case clouds = "Clouds"
-    case rain = "Rain"
-}
+//enum Icon: String, Codable {
+//    case the02D = "02d"
+//    case the03D = "03d"
+//    case the04D = "04d"
+//    case the04N = "04n"
+//    case the09D = "09d"
+//    case the10D = "10d"
+//}
+//
+//enum Main: String, Codable {
+//    case clouds = "Clouds"
+//    case rain = "Rain"
+//}
 
 // MARK: - Daily
 struct Daily: Codable {
     let dt, sunrise, sunset, moonrise: Int
     let moonset: Int
-    let moonPhase: Double
+    let moonPhase: Double?
     let temp: Temp
-    let feelsLike: FeelsLike
+    let feelsLike: FeelsLike?
     let pressure, humidity: Int
-    let dewPoint, windSpeed: Double
-    let windDeg: Int
-    let windGust: Double
+    let dewPoint, windSpeed: Double?
+    let windDeg: Int?
+    let windGust: Double?
     let weather: [Weather]
     let clouds: Int
     let pop: Double
     let rain: Double?
     let uvi: Double
-    
+
     enum CodingKeys: String, CodingKey {
         case dt, sunrise, sunset, moonrise, moonset
-        case moonPhase = "moon_phase"
+        case moonPhase
         case temp
-        case feelsLike = "feels_like"
+        case feelsLike
         case pressure, humidity
-        case dewPoint = "dew_point"
-        case windSpeed = "wind_speed"
-        case windDeg = "wind_deg"
-        case windGust = "wind_gust"
+        case dewPoint
+        case windSpeed
+        case windDeg
+        case windGust
         case weather, clouds, pop, rain, uvi
     }
 }
@@ -267,4 +267,3 @@ struct Minutely: Codable {
     let dt: Int
     let precipitation: Double
 }
-
